491e7e6edd0a1f981b879cd4ce7f145ae5ce93b8
clarkissac <sharathmanjaly@gmail.com>
2021-05-10 14:05:42 +0200

    Create data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS Sniffers;
+
+CREATE TABLE Sniffers (
+  id INT AUTO_INCREMENT  PRIMARY KEY,
+  first_name VARCHAR(250) NOT NULL,
+  last_name VARCHAR(250) NOT NULL,
+  dog_name VARCHAR(250) NOT NULL
+);
+
+INSERT INTO billionaires (first_name, last_name, dog_name) VALUES
+  ('Max', 'Mustermann', 'Raz'),
+  ('Bill', 'Gates', 'Doge'),
+  ('Folrunsho', 'Alakija', 'Hugo');
