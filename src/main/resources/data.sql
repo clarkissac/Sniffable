6f5621b95f633e4fc6ae638f9af4acbf44db7f90
clarkissac <sharathmanjaly@gmail.com>
2021-05-10 17:14:37 +0200

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,12 +2,12 @@ DROP TABLE IF EXISTS Sniffers;
 
 CREATE TABLE Sniffers (
   id INT AUTO_INCREMENT  PRIMARY KEY,
+  username VARCHAR(250) NOT NULL,
+  password VARCHAR(250) NOT NULL,
   first_name VARCHAR(250) NOT NULL,
   last_name VARCHAR(250) NOT NULL,
   dog_name VARCHAR(250) NOT NULL
 );
 
 INSERT INTO Sniffers (first_name, last_name, dog_name) VALUES
-  ('Max', 'Mustermann', 'Raz'),
-  ('Bill', 'Gates', 'Doge'),
-  ('Folrunsho', 'Alakija', 'Hugo');
+  ('MAXL','1234', 'Max', 'Mustermann', 'Raz')
