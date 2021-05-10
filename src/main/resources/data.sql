40241795b5bd95f9b0fb1173c2ec72d2b5ca959b
clarkissac <sharathmanjaly@gmail.com>
2021-05-10 22:09:02 +0200

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,9 +3,9 @@ DROP TABLE IF EXISTS Sniffers;
 CREATE TABLE Sniffers (
   id INT AUTO_INCREMENT  PRIMARY KEY,
   username VARCHAR(250) NOT NULL,
+  passwort VARCHAR(250) NOT NULL,
   first_name VARCHAR(250) NOT NULL,
   last_name VARCHAR(250) NOT NULL,
-  passwort VARCHAR(250) NULL,
   dog_name VARCHAR(250) NOT NULL,
   obj OTHER NULL
 );
