45d42260ef815aa0e82b68545494b8911285cd14
RaphaelEig <is191010@fhstp.ac.at>
2021-05-10 14:59:25 +0200

    update

--- src/main/java/at/fhstp/sniffable/SniffableApplication.java
+++ src/main/java/at/fhstp/sniffable/SniffableApplication.java
@@ -2,8 +2,10 @@ package at.fhstp.sniffable;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SniffableApplication {
 
 	public static void main(String[] args) {
