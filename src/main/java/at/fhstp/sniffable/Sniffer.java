d764578a8bbb15243251a31552ddfef72f0e004e
clarkissac <sharathmanjaly@gmail.com>
2021-05-10 15:08:57 +0200

    Update Sniffer.java

--- src/main/java/at/fhstp/sniffable/Sniffer.java
+++ src/main/java/at/fhstp/sniffable/Sniffer.java
@@ -7,16 +7,51 @@ public class Sniffer implements Subject {
 
 	protected List<Observer> observers = new ArrayList<Observer>();
 	protected String name;
-	protected String handle;
+	protected String dogname;
+	protected String firstname;
+	protected String lastname;
 	
-	public Sniffer(String name, String handle) {
+	protected String handle;
+
+	public Sniffer(String name, String firstname, String lastname, String dogname, String handle) {
 		super();
 		this.name = name;
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.dogname = dogname;
 		this.handle = "#" + handle;
+
 	}
 
 	public String getName() {
 		return name;
+	}
+
+	public String getDogname() {
+		return this.dogname;
+	}
+
+	public void setDogname(String dogname) {
+		this.dogname = dogname;
+	}
+
+	public String getFirstname() {
+		return this.firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return this.lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public void setHandle(String handle) {
+		this.handle = handle;
 	}
 
 	public void setName(String name) {
