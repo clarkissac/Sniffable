66d0fbe87a332d1b4d7c45e2ceabf3657a299600
RaphaelEig <is191010@fhstp.ac.at>
2021-05-11 23:45:58 +0200

    Update GlobalExceptionHandler.java

--- src/main/java/at/fhstp/sniffable/GlobalExceptionHandler.java
+++ src/main/java/at/fhstp/sniffable/GlobalExceptionHandler.java
@@ -3,7 +3,7 @@ package at.fhstp.sniffable;
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.multiparst.MultipartException;
+import org.springframework.web.multipart.MultipartException;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @ControllerAdvice
